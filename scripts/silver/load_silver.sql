411430712eaeec088d61f25a7e5eaffcfdd4367c
Metehan KILIÇLI <metehan.kilicli03@gmail.com>
2025-07-02 15:22:39 +0300

    Update load_silver.sql

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -1,5 +1,25 @@
-CALL silver.load_bronze();
-  
+/*
+====================================================================================
+Stored Procedure: Load Silver Layer (Bronze --> Silver)
+====================================================================================
+
+Script Purpose:
+This stored procedure performs the ETL (Extract, Transform, Load) process to
+populate the 'silver' schema tables from the 'bronze' schema.
+
+Actions Performed:
+	- Truncates Silver tables.
+	- Inserts transformed and cleansed data from Bronze into Silver tables.
+
+Parameters:
+	-None.
+	-This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+CALL silver.load_silver;
+====================================================================================
+*/
+
 CREATE OR REPLACE PROCEDURE silver.load_bronze()
 LANGUAGE plpgsql
 AS $$
